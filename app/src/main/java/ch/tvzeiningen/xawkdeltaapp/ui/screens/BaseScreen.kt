65151e6df6601ec34487bfa9970faea65938c380
Simon Freiermuth <simon.freiermuth1@students.fhnw.ch>
2023-01-02 23:08:55 +0100

    optimize imports

--- app/src/main/java/ch/tvzeiningen/xawkdeltaapp/ui/screens/BaseScreen.kt
+++ app/src/main/java/ch/tvzeiningen/xawkdeltaapp/ui/screens/BaseScreen.kt
@@ -2,8 +2,6 @@ package ch.tvzeiningen.xawkdeltaapp.ui.screens.common
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
